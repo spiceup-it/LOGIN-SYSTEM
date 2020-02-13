a479a7d8b14edcd9331adcaee43b3cfd6c336dbf
spiceup-it <37893684+spiceup-it@users.noreply.github.com>
2020-02-13 10:55:10 +0530

    new config

--- LOGIN/includes/config.php
+++ LOGIN/includes/config.php
@@ -1,17 +1,29 @@
 <?php
 
-define('DB_SERVER','localhost');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','root');
-define('DB_NAME','login');
+//define the constants
+ define("SERVERNAME","localhost");
+ define("USERNAME","root");
+ define("PASSWORD","");
+ define("DBNAME","login");
 
-$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+//create a connection to the database
+ $conn = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
 
-// if($link === false){
-//     die("ERROR: Could not connect.".mysqli_connect_error());
-// }
-// echo "Connected successfully";
-// $dbcon=mysqli_connect("localhost","root","");
-// mysqli_select_db($dbcon,"users");
+if(!$conn)
+    die("Connection failed ".mysqli_connect_error());
+else
+     echo "Connected Successfully<br>";
+
+$sql = "CREATE TABLE IF NOT EXISTS user(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(30) NOT NULL,
+        password VARCHAR(10) NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP);";
+
+    if($conn->query($sql) == true){
+        echo "Table created successfully";
+    }else{
+        echo "Error creating table".$conn->error;
+    }
 
 ?>
